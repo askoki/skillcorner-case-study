d935e1d35fb830cf8fead9a9d86c134f0fe4068b
askoki160 <arian.skoki160@gmail.com>
2025-02-23 13:03:42 +0100

    Add constants

---
+++ settings.py
@@ -0,0 +1,28 @@
+ADDITIONAL_METRICS = [
+    '_per_90', '_full_bip', '_per_60_bip',
+    '_full_tip', '_per_30_tip',
+    '_full_otip', '_per_30_otip'
+]
+
+METRIC_LABELS = {
+    '_per_90': 'Per 90 minutes',
+    '_full_bip': 'Full Ball in Play',
+    '_per_60_bip': 'Per 60 Ball in Play',
+    '_full_tip': 'Full Team in Possession',
+    '_per_30_tip': 'Per 30 minutes Team in Possession',
+    '_full_otip': 'Full Opponent Team in Possession',
+    '_per_30_otip': 'Per 30 minutes Opponent Team in Possession',
+}
+
+RUNS = {
+    'count_cross_receiver_runs_per_30_min_tip': 'Cross Receiver',
+    'count_runs_in_behind_per_30_min_tip': ' In Behind',
+    'count_runs_ahead_of_the_ball_per_30_min_tip': 'Ahead Of The Ball',
+    'count_overlap_runs_per_30_min_tip': 'Overlap',
+    'count_underlap_runs_per_30_min_tip': 'Underlap',
+    'count_support_runs_per_30_min_tip': 'Support',
+    'count_coming_short_runs_per_30_min_tip': 'Coming Short',
+    'count_dropping_off_runs_per_30_min_tip': 'Dropping Off',
+    'count_pulling_half_space_runs_per_30_min_tip': 'Pulling Half-Space',
+    'count_pulling_wide_runs_per_30_min_tip': 'Pulling Wide'
+}
