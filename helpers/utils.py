24e1ea85d4342ea80060d54c43d3b7985c70f363
askoki160 <arian.skoki160@gmail.com>
2025-02-23 11:05:34 +0100

    Add sidebar select

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,6 +1,11 @@
+from typing import Tuple
+
+import pandas as pd
 import streamlit as st
 
 from streamlit_authenticator import Authenticate
+
+from helpers.api import get_competitions, get_seasons, get_league_physical, get_league_off_ball_runs
 
 
 def authenticate():
@@ -31,3 +36,27 @@ def authenticate():
         st.error('Username/password is incorrect')
     elif st.session_state['authentication_status'] is None:
         st.warning('Please enter your username and password')
+
+
+def sidebar_selections() -> Tuple[pd.DataFrame, list, int, int]:
+    with st.sidebar:
+        competition_df = get_competitions()
+        comp_list = competition_df.name.unique()
+        selected_competition = st.selectbox('Select competition', comp_list, index=0, key='competition-select')
+
+        comp_id = competition_df.query(f'name == \"{selected_competition}\"').squeeze().id
+
+        seasons_df = get_seasons(competition_id=comp_id)
+        season_list = seasons_df.season_name.unique()
+        selected_season = st.selectbox('Select season', season_list, index=0, key='season-select')
+
+        season_id = seasons_df.query(f'season_name == \"{selected_season}\"').squeeze().season_id
+
+        selected_data_source = st.selectbox('Select data source', ['Physical', 'Off ball runs'], index=0,
+                                            key='data-source-select')
+
+    if selected_data_source == 'Physical':
+        df, metrics = get_league_physical(competition_id=comp_id, season_id=season_id)
+    else:
+        df, metrics = get_league_off_ball_runs(competition_id=comp_id, season_id=season_id)
+    return df, metrics, comp_id, season_id
