51dfe7e9721f51ca4c3409bc15bd52014926a6dd
askoki160 <arian.skoki160@gmail.com>
2025-02-23 13:02:53 +0100

    Add off the ball runs api

--- helpers/api.py
+++ helpers/api.py
@@ -7,6 +7,7 @@ from requests.auth import HTTPBasicAuth
 from skillcorner.client import SkillcornerClient
 from skillcornerviz.utils import skillcorner_physical_utils as p_utils
 from helpers.helpers import urljoin
+from settings import RUNS
 
 server_address = st.secrets.API.base_url
 username = st.secrets.API.username
@@ -83,3 +84,20 @@ def get_league_physical(competition_id: int, season_id: int) -> Tuple[pd.DataFra
     df = pd.DataFrame(data)
     metrics = p_utils.add_standard_metrics(df)
     return df, metrics
+
+
+@st.cache_resource
+def get_league_off_ball_runs(competition_id: int, season_id: int) -> Tuple[pd.DataFrame, list]:
+    data = client.get_in_possession_off_ball_runs(params={
+        'competition': competition_id, 'season': season_id,
+        'group_by': 'player,competition,team,group',
+        'average_per': '30_min_tip',
+        'playing_time__gte': 60,
+        'count_match__gte': 8,
+        'run_type': 'all,run_in_behind,run_ahead_of_the_ball,'
+                    'support_run,pulling_wide_run,coming_short_run,'
+                    'underlap_run,overlap_run,dropping_off_run,'
+                    'pulling_half_space_run,cross_receiver_run'
+    })
+    df = pd.DataFrame(data)
+    return df, RUNS.keys()
